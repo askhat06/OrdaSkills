37aa5845fd7e520f3ce53d0a8da450d930bc0786
askhat06 <askhat.akylbek.06@bk.ru>
2026-03-12 06:04:36 +0500

    chore: bootstrap Spring Boot backend

---
+++ src/test/java/kz/skills/elearning/ElearningBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package kz.skills.elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
